eda74b7353e509ac644a1884787aea31584de4d9
ee49 <mithunpatellu@gmail.com>
2024-11-16 10:45:59 -0500

    get memory of each column

--- large_data_processing/reduce_memory.py
+++ large_data_processing/reduce_memory.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+
 def summarize_columns(df):
     '''get memory usage of each column in a DataFrame
     convert all possible object types to native or numpy types. Ex: datetime, int64 to int8'''
@@ -7,3 +10,18 @@ def summarize_columns(df):
               df[c].memory_usage(deep=True) // (1024 ** 2), sep="\t")
 
     # Ex: df['column'] = df['column'].astype(np.int8)
+    # ex: df['float64'] = df['float64'].astype(np.float32) or 16
+    # remove NA values with some native type's value to reduce memory usage
+
+
+    df = pd.read_csv(
+        'yellow_tripdata.csv.gz',
+        dtype={
+            'mta_tax': np.float16
+            },
+        parse_dates=['tpep_pickup_datetime', 'tpep_dropoff_datetime'],
+        usecols=['vendor_id','tpep_pickup_datetime', 'tpep_dropoff_datetime', 'mta_tax'],
+        converters={
+        "vendor_id": lambda x: np.int8(["","1", "2"].index(x))
+        }
+    )
